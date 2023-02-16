a52d7d8a98512f63970a24218a66c763e313fcbd
Arthur <embryarthur@gmail.com>
2023-02-16 03:22:38 -0600

    tried to commit different delimeter to gcp

--- commit.sh
+++ commit.sh
@@ -16,6 +16,20 @@ sed -i '/^$/d' app.env
 # read the file into a string
 string=$(cat app.env)
 
+
+# set the delimiter to a newline character
+IFS=$'\n'
+
+# use command substitution to insert the string into the array
+array=$(echo "$string")
+
+# restore the default IFS value
+unset IFS
+
+#concatenate the array into a string with , as a separator
+string=$(printf ",%s" "${array[@]}")
+
+
 # output the string
 echo "$string"
 
